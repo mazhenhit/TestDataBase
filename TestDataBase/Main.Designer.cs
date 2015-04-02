ff09edad8758caf87b01e8a35c0baf67ec2d0045
mazhen <mazhenhit@foxmail.com>
2015-04-02 23:16:32 +0800

    init

---
+++ TestDataBase/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TestDataBase
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.departments = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.departments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(310, 31);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // departments
+            // 
+            this.departments.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.departments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.departments.Location = new System.Drawing.Point(12, 49);
+            this.departments.Name = "departments";
+            this.departments.RowTemplate.Height = 23;
+            this.departments.Size = new System.Drawing.Size(641, 265);
+            this.departments.TabIndex = 2;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(340, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(310, 31);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 326);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.departments);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.departments)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.DataGridView departments;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ TestDataBase/Main.cs
@@ -0,0 +1,204 @@
+﻿using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace TestDataBase
+{
+    public partial class Main : Form
+    {
+        private OracleConnection conn = new OracleConnection();
+        private OracleCommand cmd;
+        private OracleDataAdapter da;
+        private OracleCommandBuilder cb;
+        private DataSet ds;
+
+        public Main()
+        {
+            InitializeComponent();
+            buttonSave.Enabled = false;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //CreateOracleConnection();
+        }
+
+        public void CreateOracleConnection()
+        {
+            string connectionString = "Data Source=127.0.0.1/XE;User ID=test;PassWord=test";
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("ServerVersion: " + connection.ServerVersion
+                    + "\nDataSource: " + connection.DataSource);
+
+
+                string sql = "select stationip from station"; // C#
+                OracleCommand cmd = new OracleCommand(sql, connection);
+                cmd.CommandType = CommandType.Text;
+
+                OracleDataReader dr = cmd.ExecuteReader(); // C#
+                dr.Read();
+
+                connection.Close();   // C#
+                connection.Dispose();
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            conn.ConnectionString = "Data Source=127.0.0.1/XE;User ID=test;PassWord=test";
+
+            try
+            {
+                conn.Open();
+                buttonConnect.Enabled = false;
+
+                //slectDevice("DA");
+                deleteDevice(2);
+
+                cmd = new OracleCommand("insert into device(deviceid,devicename) values(:deviceid, :devicename)", conn);
+                cmd.CommandType = CommandType.Text;
+
+                OracleParameter deviceid = new OracleParameter();
+                deviceid.OracleDbType = OracleDbType.Decimal;
+                deviceid.Value = 14;
+                cmd.Parameters.Add(deviceid);
+
+                OracleParameter devicename = new OracleParameter();
+                devicename.OracleDbType = OracleDbType.Char;
+                devicename.Value = null;
+                cmd.Parameters.Add(devicename);
+
+
+                da = new OracleDataAdapter(cmd);
+                cb = new OracleCommandBuilder(da);
+                ds = new DataSet();
+                da.Fill(ds);
+
+                //string sql = "select * from device where deviceid < 60";
+                //cmd = new OracleCommand(sql, conn);
+                //cmd.CommandType = CommandType.Text;
+                //da = new OracleDataAdapter(cmd);
+                //cb = new OracleCommandBuilder(da);
+                //ds = new DataSet();
+                //da.Fill(ds);
+                
+                //departments.DataSource = ds.Tables[0];
+
+                buttonSave.Enabled = true;
+            }
+            catch (OracleException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 1:
+                        MessageBox.Show("Error attempting to insert duplicate data.");
+                        break;
+                    case 12560:
+                        MessageBox.Show("The database is unavailable.");
+                        break;
+                    default:
+                        MessageBox.Show("Database error: " + ex.Message.ToString());
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                //conn.Dispose();
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            da.Update(ds.Tables[0]);
+            MessageBox.Show("OK");
+        }
+
+
+        private void slectDevice(string devicename)
+        {
+            string sql = "select deviceid from device where devicename = :device_name";
+            cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleParameter device_name = new OracleParameter();
+            device_name.OracleDbType = OracleDbType.Char;
+            device_name.Value = devicename;
+            cmd.Parameters.Add(device_name);
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+
+            decimal a = dr.GetDecimal(0);
+
+            MessageBox.Show("    OK");
+        }
+
+        private void deleteDevice(int id)
+        {
+            updateChannelDevice(id);
+            updateHistoryDevice(id);
+
+            string sql = "delete from device where deviceid = :device_id";
+            cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleParameter device_id = new OracleParameter();
+            device_id.OracleDbType = OracleDbType.Decimal;
+            device_id.Value = id;
+            cmd.Parameters.Add(device_id);
+
+            da = new OracleDataAdapter(cmd);
+            cb = new OracleCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds);
+        }
+
+        private void updateChannelDevice(int id)
+        {
+            string sql = "update channel set deviceid = null where deviceid = :device_id";
+            cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleParameter device_id = new OracleParameter();
+            device_id.OracleDbType = OracleDbType.Decimal;
+            device_id.Value = id;
+            cmd.Parameters.Add(device_id);
+
+            da = new OracleDataAdapter(cmd);
+            cb = new OracleCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds);
+        }
+
+        private void updateHistoryDevice(int id)
+        {
+            string sql = "update history set deviceid = null where deviceid = :device_id";
+            cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleParameter device_id = new OracleParameter();
+            device_id.OracleDbType = OracleDbType.Decimal;
+            device_id.Value = id;
+            cmd.Parameters.Add(device_id);
+
+            da = new OracleDataAdapter(cmd);
+            cb = new OracleCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds);
+        }
+    }
+}
